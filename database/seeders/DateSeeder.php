0897761608b1e7c3754fbac5ec023edce34460da
Natan Ceballos <natan.ceballos@tutanota.com>
2024-03-20 17:00:22 -0400

    fixes to user logic and db schema

--- database/seeders/DateSeeder.php
+++ database/seeders/DateSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Date;
+use App\Models\Movie;
+use App\Models\Showtime;
 use Illuminate\Database\Seeder;
 
 class DateSeeder extends Seeder
@@ -12,16 +14,29 @@ class DateSeeder extends Seeder
      */
     public function run(): void
     {
-        // get dates from now to 2 weeks later
+        // get all movies
+        $movies = Movie::all();
+
+        // get all showtimes
+        $showtimes = Showtime::all();
+
+        // get dates from now to a week later
         $startDate = now();
-        $endDate = $startDate->copy()->addWeeks(2);
+        $endDate = $startDate->copy()->addWeeks();
 
         while ($startDate->lte($endDate)) {
             $currentDate = $startDate->copy()->format('Y-m-d');
 
-            Date::create([
+            // create date record
+            $date = Date::create([
                 'date' => $currentDate,
             ]);
+
+            // attach date to movies
+            $date->movies()->attach($movies);
+
+            // attach date to showtimes
+            $date->showtimes()->attach($showtimes);
 
             $startDate->addDay();
         }
